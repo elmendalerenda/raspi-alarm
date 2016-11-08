a4e98361fc07c9dfe9a1cb279f5dc5fda05a9c4d
Miguel Angel Fernandez <elmendalerenda@gmail.com>
2016-11-08 10:23:18 +0000

    remove rvm setup from scripts

--- scripts/autoschedule.sh
+++ scripts/autoschedule.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-`rvm cron setup`
-
 cd "$1"
 rake autoschedule > output.txt 2>&1
--- scripts/ring.sh
+++ scripts/ring.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-`rvm cron setup`
-
 cd "$1"
 rake ring > output.txt 2>&1
